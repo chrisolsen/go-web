73be07d9c768139ff5b781450803b98bb891186e
Chris Olsen <olsen.chris@gmail.com>
2025-03-30 18:09:25 -0600

    chore: refactor find/sed bash function

--- init.sh
+++ init.sh
@@ -15,14 +15,18 @@ read port
 echo "Proceed with naming? ${app_name}:${package_name} (y/N):"
 read confirm
 
+function findAndReplace () {
+    regex="s/$1/$2/g"
+    find . -type f -not -path "./.git/*" -not -path "./init.sh" -exec sed -i $regex {} \;
+}
+
 if [ "$confirm" = "y" ]; then
-    find . -type f -not -path "./.git/*" -not -path "./init.sh" -exec sed -i "s/__APP__/${app_name}/g" {} \;
-    find . -type f -not -path "./.git/*" -not -path "./init.sh" -exec sed -i "s/__DB__/${app_name}/g" {} \;
-    find . -type f -not -path "./.git/*" -not -path "./init.sh" -exec sed -i "s/__PKG__/${app_name}/g" {} \;
-    find . -type f -not -path "./.git/*" -not -path "./init.sh" -exec sed -i "s/__PORT__/${port}/g" {} \;
+    findAndReplace "__APP__" $app_name
+    findAndReplace "__DB__" $db_name
+    findAndReplace "__PKG__" $package_name
+    findAndReplace "__PORT__" $port
 
     echo "Complete!"
 else
     echo "Cancelled!"
 fi
-
