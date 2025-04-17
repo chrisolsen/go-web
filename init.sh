738f01c7aff19d527a3a9892f3ff2c6140e1b8de
Chris Olsen <olsen.chris@gmail.com>
2025-04-17 15:03:14 +0000

    remove .env file from git on project setup

--- init.sh
+++ init.sh
@@ -8,5 +8,13 @@ function findAndReplace () {
     find . -type f -not -path "./.git/*" -not -path "./init.sh" -exec sed -i $regex {} \;
 }
 
+# remove original .env file used for template dev
+git rm .env
+git commit -m "remove original .env file"
+
+# re-add env file
+touch .env
+echo "# Env variables" > .env
+
 findAndReplace "chrisolsen-goweb" $app_name
 echo "Complete!"
